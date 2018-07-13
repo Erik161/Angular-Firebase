7bc29dba817741340970f0e25895dfbf77038199
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-07-12 23:35:16 -0600

    add import enviroment.firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//FIREBASE
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+
+//enviroment config
+import {environment} from '../environments/environment';
+import { TodoComponent } from './component/todo/todo.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
