4fc786a77b05b383708d66eb6c725555f1477cc1
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-07-15 17:59:12 -0600

    add todoListArray

--- src/app/component/todo/todo.component.ts
+++ src/app/component/todo/todo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {TodoService} from '../../services/todo.service'
+
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.css']
 })
 export class TodoComponent implements OnInit {
+  todoListArray: any[];
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoService.getTodoList().snapshotChanges()
+    .subscribe(item =>{
+      this.todoListArray = [];
+      item.forEach(element =>{
+        let x = element.payload.toJSON();
+        x['$key']= element.key;
+        this.todoListArray.push(x);
+      } )
+    } )
   }
-
+  addTodo(itemTitle)
+  {
+    this.todoService.addTodo(itemTitle.value);
+    itemTitle.value = null;
+  }
 }
