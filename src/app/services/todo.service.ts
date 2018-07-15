176e0562b741cb2df2c98773e8993b36e1e75ba2
Erik Hernandez <erikestuardo_161@hotmail.com>
2018-07-14 23:48:48 -0600

    add import angularFirebaseDatabase, import AngularFireList and homework addTodo, update, remove

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
-
+import {AngularFireDatabase, AngularFireList} from 'angularfire2/database';
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
   constructor() { }
+
+  getTodoList()
+  {
+
+  }
+
+  addTodo()
+  {
+
+  }
+
+
+ updateTodo()
+ {
+
+ }
+
+
+removeTodo()
+{
+
+}
+
+
+
 }
